b418c50d936d90236d508674b07125a32d6ae808
Manuel Garcia-Arevalo Peinado <mgarpei2905@iesfuengirola1.es>
2026-01-21 20:10:05 +0100

    Manuel Garcia-Arevalo Peinado: Crear una nueva rama y en ella, modificar el archivo examen.py añadiendo una nueva linea con mi nombre

--- examen.py
+++ examen.py
@@ -2,5 +2,5 @@
 def iniciar_sistema():
     print("Iniciando sistema de evaluación...")
     print("Versión 1.0 - Conexión establecida.")
-
+    print("Manuel Garcia-Arévalo Peinado")
 iniciar_sistema()
